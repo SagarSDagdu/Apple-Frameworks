ba9f79714b04581461a0541a1492593f0cb756b1
Sagar Dagdu <shags032@gmail.com>
2021-06-03 17:32:29 +0530

    Add framework view

---
+++ Apple-Frameworks/Apple-Frameworks/FrameworkView.swift
@@ -0,0 +1,32 @@
+//
+//  FrameworkView.swift
+//  Apple-Frameworks
+//
+//  Created by Sagar Dagdu on 03/06/21.
+//
+
+import SwiftUI
+
+struct FrameworkView: View {
+    var framework: Framework
+    
+    var body: some View {
+        VStack {
+            Image(framework.imageName)
+                .resizable()
+                .frame(width: 90, height: 90)
+                .aspectRatio(contentMode: .fit)
+            Text(framework.name)
+                .font(.title2)
+                .fontWeight(.semibold)
+                .scaledToFit()
+                .minimumScaleFactor(0.5)
+        }
+    }
+}
+
+struct FrameworkView_Previews: PreviewProvider {
+    static var previews: some View {
+        FrameworkView(framework: MockData.frameworks.first!)
+    }
+}
